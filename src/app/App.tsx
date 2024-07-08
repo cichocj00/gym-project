cdbfa9559855ff636fd82adc916d5687c16e16ba
cichocj00 <cichocjagh@gmail.com>
2024-07-08 11:30:46 +0200

    add theme toggle

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,30 @@
+import { useThemeStore } from '@/store/theme';
+import { useEffect } from 'react';
+
 import AppProvider from './AppProvider';
 import AppRouter from './routes/AppRouter';
 
 function App() {
+  const theme = useThemeStore((state) => state.theme);
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+
+    root.classList.remove('light', 'dark');
+
+    if (theme === 'system') {
+      const systemTheme = window.matchMedia('(prefers-color-scheme: dark)')
+        .matches
+        ? 'dark'
+        : 'light';
+
+      root.classList.add(systemTheme);
+      return;
+    }
+
+    root.classList.add(theme);
+  }, [theme]);
+
   return (
     <AppProvider>
       <AppRouter />
--- src/app/routes/Homepage.tsx
+++ src/app/routes/Homepage.tsx
@@ -1,5 +1,12 @@
+import ThemeToggle from '@/components/ThemeToggle';
+
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <h1 className="">Homepage</h1>
+      <ThemeToggle />
+    </div>
+  );
 };
 
 export default Homepage;
